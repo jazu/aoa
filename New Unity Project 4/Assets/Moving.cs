aff782c8e000b8dd6bca2292ffb73c54a22f65f8
unknown <asentaja@WS1200586.bc.local>
2015-02-12 13:28:18 +0200

    Moi oon Teemu

---
+++ New Unity Project 4/Assets/Moving.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moving : MonoBehaviour
+{
+	public float f_speed;
+	public float f_jumpSpeed;
+	public float f_gravity;
+	private Vector3 v3_moveDirection = Vector3.zero;
+	private CharacterController controller;
+	
+	void Awake ()
+	{
+		controller = GetComponent<CharacterController>();
+	}
+	
+	void Update ()
+	{
+		if (controller.isGrounded)
+		{    
+			v3_moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+			v3_moveDirection *= f_speed;
+			
+			if (Input.GetButton("Jump"))
+			{
+				v3_moveDirection.y = f_jumpSpeed;
+			}
+		}
+		
+		v3_moveDirection.y -= f_gravity * Time.deltaTime;
+		controller.Move(v3_moveDirection * Time.deltaTime);
+	}
+	
+}
