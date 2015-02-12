dfa5363ad9e362c006caaaadacdda5edf825cd31
unknown <s1200522@WS1200522.bc.local>
2015-02-12 08:47:07 +0200

    Added whole unity project -J

---
+++ auroria/Assets/Scripts/BackGroundScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackGroundScript : MonoBehaviour {
+
+	public float speed = 0;
+	public static BackGroundScript current;
+
+	float pos = 0;
+	// Use this for initialization
+	void Start () {
+		current = this;
+		if (Application.loadedLevelName == "options") {
+
+				}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Application.loadedLevelName == "options") {
+						
+								pos += speed;
+								if (pos > 1.0f)
+										pos -= 1.0f;
+
+								renderer.material.mainTextureOffset = new Vector2 (pos, 0);
+						
+				} else {
+						pos += speed;
+						if (pos > 1.0f)
+								pos -= 1.0f;
+			
+						renderer.material.mainTextureOffset = new Vector2 (pos, 0);
+				}
+	}
+}
---
+++ auroria/Assets/Scripts/menuLoadLevels.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class menuLoadLevels : MonoBehaviour {
+	
+	public Button musicBtn;
+	public Sprite musicOff;
+	public Sprite musicOn;
+
+    public void OnClickOptions(){
+  		Application.LoadLevel("options");
+ }
+    public void OnClickStartMenu(){
+  		Application.LoadLevel("start");
+ }
+	public void onClickChooseLevel() {
+		Application.LoadLevel("chooseLevel");
+ }
+	public void musicToggle(){
+		if (musicBtn.image.sprite == musicOff) {
+			musicBtn.image.sprite = musicOn;
+		} else {
+
+			musicBtn.image.sprite=musicOff;		
+		}
+	}
+}
